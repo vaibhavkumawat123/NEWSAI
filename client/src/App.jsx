b7a7b12faf34875b0862c60468112ac6fba8f3c6
Vaibhav <vaibhavraj771@gmail.com>
2025-02-13 18:21:10 +0530

    vaibhav

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import {Routes , Route} from "react-router-dom"
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+    <Route />
+    </Routes>
+    </div>
+
+  )
+}
+
+export default App
